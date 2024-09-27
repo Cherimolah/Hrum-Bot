8b441cc046e7421fcf36db57bc5213e94a145e5c
Cherimolah <inkustregister@mail.ru>
2024-09-27 08:38:21 +0500

    Fix throttling error

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import asyncio
+import random
 from urllib.parse import unquote
 from datetime import datetime, timedelta
 
@@ -34,29 +35,32 @@ class Bot:
     async def run(self):
         await self.tg_client.connect()
         while True:
-            logger.info(f'{self.tg_client.name} | Session validated')
-            web_params = await self.get_init_params()
-            logger.info(f'{self.tg_client.name} | Web View params got')
-            api_key = unquote(web_params.split('&hash=', maxsplit=1)[1])
-            self.http_client.api_key = api_key
-            auth = await self.http_client.auth(web_params)
-            if auth['success']:
-                logger.success(f'{self.tg_client.name} | Auth successful')
-            else:
-                logger.error(f'{self.tg_client.name} | Auth failed')
-                return
-            info = await self.http_client.info()
-            update_date = info['data']['hero']['updateDate']
-            update_date = datetime.strptime(update_date, '%Y-%m-%d %H:%M:%S')
-            now = datetime.utcnow()
-            tomorrow = now + timedelta(days=1)
-            tomorrow = datetime(tomorrow.year, tomorrow.month, tomorrow.day, 7, 0, 0)
-            if (now - update_date).total_seconds() > 86400:
-                cookies = (await self.http_client.open())['data']['history']
-                cookies.sort(key=lambda x: datetime.strptime(x['updateDate'], '%Y-%m-%d %H:%M:%S'), reverse=True)
-                logger.success(f'{self.tg_client.name} | Cookie opened: {cookies[0]["text"]}')
-                logger.info(f'{self.tg_client.name} | Cookie will be available in {(tomorrow - now).total_seconds()} seconds')
-                await asyncio.sleep((tomorrow - now).total_seconds() + 2)
-            else:
-                logger.info(f'{self.tg_client.name} | Cookie will be available in {(tomorrow - now).total_seconds()} seconds')
-                await asyncio.sleep((tomorrow - now).total_seconds() + 2)
+            try:
+                logger.info(f'{self.tg_client.name} | Session validated')
+                web_params = await self.get_init_params()
+                logger.info(f'{self.tg_client.name} | Web View params got')
+                api_key = unquote(web_params.split('&hash=', maxsplit=1)[1])
+                self.http_client.api_key = api_key
+                auth = await self.http_client.auth(web_params)
+                if auth['success']:
+                    logger.success(f'{self.tg_client.name} | Auth successful')
+                else:
+                    logger.error(f'{self.tg_client.name} | Auth failed')
+                    return
+                info = await self.http_client.info()
+                update_date = info['data']['hero']['updateDate']
+                update_date = datetime.strptime(update_date, '%Y-%m-%d %H:%M:%S')
+                now = datetime.utcnow()
+                tomorrow = now + timedelta(days=1)
+                tomorrow = datetime(tomorrow.year, tomorrow.month, tomorrow.day, 7, 0, 0)
+                if (now - update_date).total_seconds() > 86400:
+                    cookies = (await self.http_client.open())['data']['history']
+                    cookies.sort(key=lambda x: datetime.strptime(x['updateDate'], '%Y-%m-%d %H:%M:%S'), reverse=True)
+                    logger.success(f'{self.tg_client.name} | Cookie opened: {cookies[0]["text"]}')
+                    logger.info(f'{self.tg_client.name} | Cookie will be available in {(tomorrow - now).total_seconds()} seconds')
+                    await asyncio.sleep((tomorrow - now).total_seconds() + random.randint(30, 120))
+                else:
+                    logger.info(f'{self.tg_client.name} | Cookie will be available in {(tomorrow - now).total_seconds()} seconds')
+                    await asyncio.sleep((tomorrow - now).total_seconds() + random.randint(30, 120))
+            except:
+                await asyncio.sleep(10)
